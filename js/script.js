8e42bdcb4f8eb94054b367c922114ae36e6bf038
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-10 17:34:02 -0700

    Initial commit: change background color of footer, change h2 tagline, increase line height and font size of the address, and emphasize the words 'every day' in the contact section header

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var footer = document.querySelector("footer");
+// console.log(footer);
+footer.style.backgroundColor = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+// console.log(tagline);
+tagline.innerText = "Flowers designed with your thoughts in mind";
+
+var address = document.querySelector("address");
+// console.log(address);
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+
+var contactEveryDay = document.querySelector("h3");
+// console.log(contactEveryDay);
+contactEveryDay.innerHTML =
+  "We're here for you <em>every day</em> of the week.";
